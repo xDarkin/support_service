ba927b3185f632436dcdde4c4fbda03b85770653
xDarkin <pavel.meleshin@gmail.com>
2023-01-03 18:30:27 +0200

    update admin panel

--- src/users/managers.py
+++ src/users/managers.py
@@ -3,26 +3,29 @@ from django.contrib.auth.models import UserManager as _UserManager
 from users.constants.roles import Role
 
 
+def _create_user(self, email, password, **extra_fields):
+    """
+    Create and save a user with the given username, email, and password.
+    """
+    email = self.normalize_email(email)
+    user = self.model(email=email, **extra_fields)
+    user.password = make_password(password)
+    user.save(using=self._db)
+
+    return user
+
+
 class UserManager(_UserManager):
     def create_superuser(self, email=None, password=None, **extra_fields):
         extra_fields["is_staff"] = True
+        extra_fields["is_superuser"] = True
         extra_fields["role"] = Role.ADMIN
 
         return self._create_user(email, password, **extra_fields)
 
-    def _create_user(self, email, password, **extra_fields):
-        """
-        Create and save a user with the given username, email, and password.
-        """
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
-        user.password = make_password(password)
-        user.save(using=self._db)
-
-        return user
-
     def create_user(self, email=None, password=None, **extra_fields):
         extra_fields["is_staff"] = False
+        extra_fields["is_superuser"] = False
         extra_fields["role"] = Role.USER
 
         return self._create_user(email, password, **extra_fields)
--- src/users/models.py
+++ src/users/models.py
@@ -1,9 +1,10 @@
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
+from users.constants.roles import Role
 from users.managers import UserManager
 
 
-class User(AbstractBaseUser):
+class User(AbstractBaseUser, PermissionsMixin):
 
     id = models.AutoField(primary_key=True)
     email = models.EmailField(unique=True)
@@ -14,7 +15,7 @@ class User(AbstractBaseUser):
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
-    role = models.CharField(max_length=7, choices=[], default="user")
+    role = models.CharField(max_length=7, choices=Role.values(), default="user")
 
     EMAIL_FIELD = "email"
     USERNAME_FIELD = "email"
@@ -36,3 +37,8 @@ class User(AbstractBaseUser):
     def get_short_name(self):
         """Return the short name for the user."""
         return self.first_name
+
+    def __str__(self) -> str:
+        if self.first_name and self.last_name:
+            return f"{self.first_name} {self.last_name}"
+        return self.email
